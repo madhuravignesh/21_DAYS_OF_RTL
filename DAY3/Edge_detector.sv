327588e3e343ec5d41aeee7576e6f2735de1eca3
Madhura Chekodu <cmadhura08@gmail.com>
2025-07-23 23:55:01 +0530

    SV code for rising and falling edge detection

--- DAY3/Edge_detector.sv
+++ DAY3/Edge_detector.sv
@@ -1,1 +1,27 @@
+// System verilog code for rising and falling edge detector
 
+module edge_detector (
+  input logic clk,
+  input logic reset_n,
+  input logic x_i,
+  output logic rising_edge_o,
+  output logic falling_edge_o
+);
+  
+  logic x_q;
+  
+  always_ff @(posedge clk) begin
+    if (~reset_n) begin
+      x_q <= '0;
+    end else begin
+      x_q <= x_i;
+    end
+  end
+  
+  assign rising_edge_o = x_i & ~x_q;
+  
+  assign falling_edge_o = ~x_i & x_q;
+  
+  
+endmodule
+    
