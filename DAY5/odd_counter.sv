9212014ed7a1b4f0652338bf5240f79a9aa8b6d6
Madhura Chekodu <cmadhura08@gmail.com>
2025-07-25 23:15:23 +0530

    SV code for an 8 bit odd counter

--- DAY5/odd_counter.sv
+++ DAY5/odd_counter.sv
@@ -1,1 +1,21 @@
+module odd_counter (
+  input logic clk,
+  input logic reset_n,
+  output logic [7:0] count_o
+);
+  
+  logic [7:0] nxt_count;
+  
+  always_ff @(posedge clk) begin
+    if (~reset_n) begin
+      count_o <= 8'h1;
+    end else begin
+      count_o <= nxt_count;
+    end
+  end
+  
+  assign nxt_count = count_o + 8'h2;
+  
+endmodule
+
 
