eea2c5f6cb46435cd23d68bef5f9634231a4a485
Madhura Chekodu <cmadhura08@gmail.com>
2025-07-25 23:16:12 +0530

    TB to verify 8 bit odd counter

---
+++ DAY5/odd_counter_tb.sv
@@ -0,0 +1,26 @@
+module odd_counter_tb ();
+  logic clk;
+  logic reset_n;
+  logic [7:0]  count_o;
+  
+  initial begin
+    $dumpfile ("dump.vcd");
+    $dumpvars (0,odd_counter_tb);
+  end
+  
+  initial begin
+    clk = 1'b0;
+  end
+ 
+  always #5 clk = ~clk;
+ 
+  
+   initial begin
+    reset_n = 1'b0;
+    #12;
+    reset_n = 1'b1;
+    #200;
+    $finish();
+  end
+  
+endmodule
