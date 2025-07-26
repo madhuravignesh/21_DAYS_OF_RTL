90deaa1e0ad51b9a8aa74413997b5ad66fb56004
Madhura Chekodu <cmadhura08@gmail.com>
2025-07-26 22:58:16 +0530

    SV code to model a simple 4 bit shift register

--- DAY6/shift_register.sv
+++ DAY6/shift_register.sv
@@ -1,1 +1,22 @@
+// System code for a simple shift register
 
+module shift_reg (
+  input wire clk,
+  input wire reset_n,
+  input wire x_i,
+  output logic [3:0] sr_o
+);
+  
+  logic [3:0] next_val;
+  
+  always_ff @(posedge clk) begin
+    if (~reset_n) begin
+      sr_o <= '0;
+    end else begin
+      sr_o <= next_val;
+    end
+  end
+  
+   assign next_val = {sr_o[2:0],x_i};
+  
+endmodule
